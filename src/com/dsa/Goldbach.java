852db25ec576f59f96404f1fe1485a4953a9fdf8
vimal <vimalkjoshi92@gmail.com>
2026-01-31 16:29:47 +0530

    Goldbach's Conjecture

--- src/com/dsa/Goldbach.java
+++ src/com/dsa/Goldbach.java
@@ -1,35 +1,34 @@
 package com.dsa;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Goldbach {
 
 	public static void main(String[] args) {
-
-		String s = gold(9);
-		System.out.println(s);
-	}
-
-	public static String gold(int n) {
-		if (n <= 2 || n%2 !=0) {
-			return "Goldbach's conjecture not applicable";
-		}
-		for (int i = 2; i <= n / 2; i++) {
-			if (n%2==0 && isPrime(i) && isPrime(n - i)) {
-				return (n + "=" + i + "+" + (n - i));
+		int n = 42;
+		int[] arr = new int[n+1];
+		List<Integer> s = new ArrayList<>();
+		for(int i=2;i<=n;i++) {
+			if(arr[i]==0) {
+				for(int k = 2*i;k<=n;k=k+i) {
+					arr[k]=1;
+				}
+				s.add(i);
 			}
 		}
-		return "Goldbach's conjecture is wrong.";
-	}
-
-	public static boolean isPrime(int n) {
-		if (n <= 1) {
-			return false;
+		int i = 0;
+		int j = s.size()-1;
+		while(i<=j) {
+		if(s.get(i)+s.get(j) == n) {
+			System.out.println(n+" = "+s.get(i)+"+"+s.get(j));
+			return;
+		}else if(s.get(i)+s.get(j) > n) {
+			j--;
 		}
-		for (int i = 2; i <= Math.sqrt(n); i++) {
-			if (n % i == 0) {
-				return false;
-			}
+		else {
+			i++;
 		}
-		return true;
+		}
 	}
-
 }
