df7c7f48d3feab886feea1eb16770ebd3d46bc1d
vimal <vimalkjoshi92@gmail.com>
2026-01-31 12:17:56 +0530

    factorial submmitted code

--- src/com/dsa/Factorial.java
+++ src/com/dsa/Factorial.java
@@ -1,21 +1,27 @@
 package com.dsa;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
 public class Factorial {
+	    public static void main(String[] args) throws IOException {
+	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        
+	        int T = Integer.parseInt(br.readLine().trim());
+	        
+	        while (T-- > 0) {
+	            long n = Long.parseLong(br.readLine().trim());
+	            System.out.println(trailingZeros(n));
+	        }
+	    }
 
-	public static void main(String[] args) {
-		int ans = totalZero(8735373);
-		System.out.println(ans);
-
+	    public static long trailingZeros(long num) {
+	        long result = 0;
+	        while (num > 0) {
+	            num /= 5;
+	            result += num;
+	        }
+	        return result;
+	    }
 	}
-	
-	public static int totalZero(int num) {
-		int result = 0;
-		while(num>1) {
-			int n = num/5;
-			result += n;
-			num = n;
-		}
-		return result;
-	}
-
-}
