5b84506edc6ba7343c352eced0ab01af9c7ed288
vimal <vimalkjoshi92@gmail.com>
2026-02-01 13:12:17 +0530

    square diffrences

--- src/com/dsa/SquareDifference.java
+++ src/com/dsa/SquareDifference.java
@@ -1,25 +1,133 @@
 package com.dsa;
 
-public class SquareDifference {
+	import java.io.OutputStream;
+	import java.io.IOException;
+	import java.io.InputStream;
+	import java.io.PrintWriter;
+	import java.io.PrintStream;
+	import java.util.InputMismatchException;
+	import java.io.IOException;
+	import java.io.InputStream;
 
-	public static void main(String[] args) {
-		int[] primes = new int[10000000];
-		for(int i =2;i<primes.length;i++) {
-			if(primes[i] == 0) {
-				for(int j=i*2;j<primes.length;j=j+i) {
-					primes[j]=1;
-				}
-			}
-		}
-		int a = 34;
-		int b = 33;
-		if(a-b == 1 && primes[a+b]==0) {
-			System.out.println("YES");
-		}else {
-			System.out.println("NO");
-		}
-		
+	/**
+	 * Built using CHelper plug-in
+	 * Actual solution is at the top
+	 */
+	public class SquareDifference {
+	    public static void main(String[] args) {
+	        InputStream inputStream = System.in;
+	        OutputStream outputStream = System.out;
+	        InputReader in = new InputReader(inputStream);
+	        PrintWriter out = new PrintWriter(outputStream);
+	        Sdiff solver = new Sdiff();
+	        int testCount = Integer.parseInt(in.next());
+	        for (int i = 1; i <= testCount; i++)
+	            solver.solve(i, in, out);
+	        out.close();
+	    }
+
+	    static class Sdiff {
+	        public void solve(int testNumber, InputReader in, PrintWriter out) {
+	            long a = in.readLong();
+	            long b = in.readLong();
+	            if ((a - b) == 1 && isPrime(a + b)) {
+	                System.out.println("YES");
+	            } else {
+	                System.out.println("NO");
+	            }
+
+
+	        }
+
+	        public static boolean isPrime(long n) {
+	            for (long i = 2; i * i <= n; i++) {
+	                if (n % i == 0)
+	                    return false;
+	            }
+	            return true;
+	        }
+
+	    }
+
+	    static class InputReader {
+	        private InputStream stream;
+	        private byte[] buf = new byte[1024];
+	        private int curChar;
+	        private int numChars;
+	        private InputReader.SpaceCharFilter filter;
+
+	        public InputReader(InputStream stream) {
+	            this.stream = stream;
+	        }
+
+	        public int read() {
+	            if (numChars == -1)
+	                throw new InputMismatchException();
+	            if (curChar >= numChars) {
+	                curChar = 0;
+	                try {
+	                    numChars = stream.read(buf);
+	                } catch (IOException e) {
+	                    throw new InputMismatchException();
+	                }
+	                if (numChars <= 0)
+	                    return -1;
+	            }
+	            return buf[curChar++];
+	        }
+
+	        public long readLong() {
+	            int c = read();
+	            while (isSpaceChar(c))
+	                c = read();
+	            int sgn = 1;
+	            if (c == '-') {
+	                sgn = -1;
+	                c = read();
+	            }
+	            long res = 0;
+	            do {
+	                if (c < '0' || c > '9')
+	                    throw new InputMismatchException();
+	                res *= 10;
+	                res += c - '0';
+	                c = read();
+	            } while (!isSpaceChar(c));
+	            return res * sgn;
+	        }
+
+	        public String readString() {
+	            int c = read();
+	            while (isSpaceChar(c))
+	                c = read();
+	            StringBuilder res = new StringBuilder();
+	            do {
+	                res.appendCodePoint(c);
+	                c = read();
+	            } while (!isSpaceChar(c));
+	            return res.toString();
+	        }
+
+	        public boolean isSpaceChar(int c) {
+	            if (filter != null)
+	                return filter.isSpaceChar(c);
+	            return isWhitespace(c);
+	        }
+
+	        public static boolean isWhitespace(int c) {
+	            return c == ' ' || c == '\n' || c == '\r' || c == '\t' || c == -1;
+	        }
+
+	        public String next() {
+	            return readString();
+	        }
+
+	        public interface SpaceCharFilter {
+	            public boolean isSpaceChar(int ch);
+
+	        }
+
+	    }
 	}
-	
 
-}
+
