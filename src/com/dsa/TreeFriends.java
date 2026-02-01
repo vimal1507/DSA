5f0b3b3f566c2924503056a07220abb053437238
vimal <vimalkjoshi92@gmail.com>
2026-02-01 22:44:45 +0530

    three friends

---
+++ src/com/dsa/TreeFriends.java
@@ -0,0 +1,19 @@
+package main;
+
+import input.InputReader;
+import java.io.PrintWriter;
+
+public class TreeFriends {
+    public void solve(int testNumber, InputReader in, PrintWriter out) {
+        int x = in.readInt();
+        int y = in.readInt();
+        int z = in.readInt();
+
+        if (x+y==z || y+z==x || z+x==y) {
+            System.out.println("yes");
+        }
+        else{
+            System.out.println("no");
+        }
+    }
+}
