c617722da60eddee921fc327b425002ba8b3fa72
vimal <vimalkjoshi92@gmail.com>
2026-02-04 15:20:43 +0530

    backspace string commpare

---
+++ src/com/dsa/BackSpaceStringCompare.java
@@ -0,0 +1,41 @@
+package com.dsa;
+
+public class BackSpaceStringCompare {
+
+	public static void main(String[] args) {
+		System.out.println(backspaceCompare("ad#c","ab#c"));
+
+	}
+
+	public static boolean backspaceCompare(String s, String t) {
+		StringBuilder sbs = new StringBuilder();
+		StringBuilder sbt = new StringBuilder();
+		for (int i = 0; i < s.length(); i++) {
+			if (s.charAt(i) != '#') {
+				sbs.append(s.charAt(i));
+			} else {
+				if (sbs.length() > 0) {
+					sbs.deleteCharAt(sbs.length() - 1);
+				}
+			}
+		}
+
+		for (int i = 0; i < t.length(); i++) {
+			if (t.charAt(i) != '#') {
+				sbt.append(t.charAt(i));
+			} else {
+				if (sbt.length() > 0) {
+					sbt.deleteCharAt(sbt.length() - 1);
+				}
+			}
+		}
+
+		if (sbs.toString().equals(sbt.toString())) {
+			return true;
+		} else {
+			return false;
+		}
+
+	}
+
+}
