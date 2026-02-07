0a1e51676f2b3767e9e7955fbf0239a6913c8327
vimal <vimalkjoshi92@gmail.com>
2026-02-07 12:08:30 +0530

    using queue distance of given node

--- src/com/dsa/DistanceFromRootToGivenNode.java
+++ src/com/dsa/DistanceFromRootToGivenNode.java
@@ -1,52 +1,74 @@
 package com.dsa;
 
-import java.util.Stack;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class DistanceFromRootToGivenNode {
-	
-    static class TreeNode {
-        int val;
-        TreeNode left, right;
-        TreeNode(int val) { this.val = val; }
-    }
 
-	public static void main(String[] args) {
-		
-		TreeNode root = new TreeNode(20);
-        root.left = new TreeNode(40);
-        root.right = new TreeNode(60);
-        root.left.left = new TreeNode(80); 
-        
-        
-        System.out.println(distance(root,40));
-		}
+	// Java program to find distance of a given 
+	// node from root. 
 
-	
-	public static int distance(TreeNode node, int x) {
-		
-		if(node == null)
-			return -1;
-		Stack<TreeNode> st = new Stack<>();
-		Stack<Integer> sd = new Stack<>();
-		st.push(node);
-		sd.push(1);
-		while(!st.isEmpty()) {
-			TreeNode currNode = st.pop();
-			int temp = sd.pop();
-			if(currNode.val == x) {
-				return temp-1;
-			}
-			temp++;
-			if(currNode.left != null) {
-				st.push(currNode.left);
-				sd.push(temp);
-			}
-			
-			if(currNode.right != null) {
-				st.push(currNode.right);
-				sd.push(temp);
-			}
-		}
-		return -1;
+
+	// A Binary Tree Node 
+	static class Node 
+	{ 
+	    int data; 
+	    Node left, right; 
 	}
-}
+
+	// A utility function to create a new Binary 
+	// Tree Node 
+	static Node newNode(int item) 
+	{ 
+	    Node temp = new Node(); 
+	    temp.data = item; 
+	    temp.left = null;
+	    temp.right = null; 
+	    return temp; 
+	} 
+
+	// Returns -1 if x doesn't exist in tree. Else 
+	// returns distance of x from root 
+	static int findDistance(Node root, int x) 
+	{ 
+	   Queue<Node> q = new LinkedList<>();
+	   q.offer(root);
+	   int dis = 0;
+	   while(!q.isEmpty()) {
+		   int size = q.size();
+		   for(int i=0; i<size; i++) {
+			   Node currNode = q.poll();
+			   if(currNode.data == x) {
+				   return dis;
+			   }
+			   
+			   if(currNode.left != null) {
+				   q.offer(currNode.left);
+			   }
+			   
+			   if(currNode.right != null) {
+				   q.offer(currNode.right);
+			   }
+		   }
+		   
+		   dis++;
+	   }
+	   return -1;
+	} 
+
+	// Driver Program to test above functions 
+	public static void main(String[] args) 
+	{ 
+	    Node root = newNode(5); 
+	    root.left = newNode(10); 
+	    root.right = newNode(15); 
+	    root.left.left = newNode(20); 
+	    root.left.right = newNode(25); 
+	    root.left.right.right = newNode(45); 
+	    root.right.left = newNode(30); 
+	    root.right.right = newNode(35); 
+
+	    System.out.println(findDistance(root, 10)); 
+	}
+	}
+
