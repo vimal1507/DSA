7df768308e3830b3518770757bfc598076dc67ce
vimal <vimalkjoshi92@gmail.com>
2026-02-07 11:28:42 +0530

    use queue for binary transversal

--- src/com/dsa/BinaryTreeLevelOrderTraversal.java
+++ src/com/dsa/BinaryTreeLevelOrderTraversal.java
@@ -1,8 +1,9 @@
 package com.dsa;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
-import java.util.Stack;
+import java.util.Queue;
 
 public class BinaryTreeLevelOrderTraversal {
 
@@ -26,42 +27,51 @@ public class BinaryTreeLevelOrderTraversal {
 	 *     }
 	 * }
 	 */
+	/**
+	 * Definition for a binary tree node.
+	 * public class TreeNode {
+	 *     int val;
+	 *     TreeNode left;
+	 *     TreeNode right;
+	 *     TreeNode() {}
+	 *     TreeNode(int val) { this.val = val; }
+	 *     TreeNode(int val, TreeNode left, TreeNode right) {
+	 *         this.val = val;
+	 *         this.left = left;
+	 *         this.right = right;
+	 *     }
+	 * }
+	 */
 	    public List<List<Integer>> levelOrder(TreeNode root) {
-
-	        Stack<TreeNode> s = new Stack<>();
+	        Queue <TreeNode> q = new LinkedList<>();
 	        List<List<Integer>> al = new ArrayList<>();
 	        
 	        if(root == null){
 	            return al;
 	        }
-	        
-	        s.push(root);
 
-	        while(!s.isEmpty()){
-	            List<Integer> tempList = new ArrayList<>();
-	            int size = s.size();
-	            Stack<TreeNode> tempStack = new Stack<>();
+	        q.offer(root);
 
-	            for(int i=0; i<size; i++){
-	                TreeNode currNode = s.pop();
-	                tempList.add(currNode.val);
-	                
+	        while(!q.isEmpty()){
+	            int totalNode = q.size();
+	            List<Integer> currLavelChild = new ArrayList<>();
+
+	            for(int i=0; i<totalNode; i++){
+	                TreeNode currNode = q.poll();
+	                currLavelChild.add(currNode.val);
+
 	                if(currNode.left != null){
-	                    tempStack.push(currNode.left);
+	                    q.offer(currNode.left);
 	                }
 
 	                if(currNode.right != null){
-	                    tempStack.push(currNode.right);
-	                }
-	            }
-
-	            al.add(tempList);
-
-	            while(!tempStack.isEmpty()){
-	                s.push(tempStack.pop());
-	            }
+	                    q.offer(currNode.right);
+	                }              
+	            } 
+	             al.add(currLavelChild);
 	        }
+	        
+	        
 	        return al;
 	    }
-
 }
