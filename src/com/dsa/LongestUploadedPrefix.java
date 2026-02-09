23d6cd64243996f924e12d42d9dfc5d2a0cbaac8
vimal <vimalkjoshi92@gmail.com>
2026-02-09 21:22:03 +0530

    longest uploaded prefix

--- src/com/dsa/LongestUploadedPrefix.java
+++ src/com/dsa/LongestUploadedPrefix.java
@@ -1,5 +1,8 @@
 package com.dsa;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class LongestUploadedPrefix {
 
 	public static void main(String[] args) {
@@ -8,3 +11,29 @@ public class LongestUploadedPrefix {
 	}
 
 }
+
+class LUPrefix {
+    Set<Integer> set;
+    int max = 0;
+    public LUPrefix(int n) {
+        set = new HashSet<>();
+    }
+    
+    public void upload(int video) {
+        set.add(video);
+        while(set.contains(max+1)){
+            max++;
+        }
+    }
+    
+    public int longest() {
+        return max;
+    }
+}
+
+/**
+ * Your LUPrefix object will be instantiated and called as such:
+ * LUPrefix obj = new LUPrefix(n);
+ * obj.upload(video);
+ * int param_2 = obj.longest();
+ */
