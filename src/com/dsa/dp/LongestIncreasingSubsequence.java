20cd31f1758a5293c90e5f74d9b18d66e4ebdb2c
vimal <vimalkjoshi92@gmail.com>
2026-02-21 22:24:43 +0530

    Longest Increasing Subsequence

---
+++ src/com/dsa/dp/LongestIncreasingSubsequence.java
@@ -0,0 +1,37 @@
+package com.dsa.dp;
+
+import java.util.Arrays;
+
+public class LongestIncreasingSubsequence {
+
+	public static void main(String[] args) {
+		int l = lengthOfLIS(new int[] {10,9,2,5,3,7,101,18});
+		System.out.println(l);
+
+	}
+	
+	public static int lengthOfLIS(int[] nums) {
+        int[][] dp = new int[nums.length+1][nums.length];
+        for(int[] a : dp){
+            Arrays.fill(a,-1);
+        }
+        return lis(nums,-1,0,dp);
+    }
+
+    private static int lis(int[] nums, int pre, int cur, int[][] dp){
+        if(cur == nums.length)
+        return 0;
+        if(dp[pre+1][cur] != -1)
+        return dp[pre+1][cur];
+
+
+        int case1 = lis(nums,pre,cur+1,dp);
+        int case2 = 0;
+        if(pre == -1 || nums[pre] < nums[cur]){
+            case2 = 1+lis(nums,cur,cur+1,dp);
+        }
+
+        return dp[pre+1][cur] = Math.max(case1,case2);
+    }
+
+}
