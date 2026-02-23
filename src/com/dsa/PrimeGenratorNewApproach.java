3f2d1b91dfe3733715150023696ba5d9a8fb55f2
vimal <vimalkjoshi92@gmail.com>
2026-02-23 13:22:36 +0530

    prime generator

---
+++ src/com/dsa/PrimeGenratorNewApproach.java
@@ -0,0 +1,31 @@
+package com.dsa;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PrimeGenratorNewApproach {
+
+	public static void main(String[] args) {
+		 int a = 3;
+	     int b = 5;
+	     List<Integer> al = new ArrayList<Integer>();
+	    A: for(int j=a;j<=b;j++) {
+	        if(j==1)
+	            continue A;
+	         for (int i = 2; i * i <= j; i++) {
+	             if(j%i == 0) {
+	                 continue A;
+	             }
+
+	         }
+	         al.add(j);
+	     }
+	     for(Integer i : al) {
+	         System.out.println(i);
+	     }
+	     System.out.println();
+	
+	
+ }
+
+}
