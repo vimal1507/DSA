977ad5f447808558df5403f4504ae5f5ae92a302
vimal <vimalkjoshi92@gmail.com>
2026-02-26 21:19:40 +0530

    grpah is a valid tree

---
+++ src/com/dsa/graph/GraphValidTree.java
@@ -0,0 +1,51 @@
+package com.dsa.graph;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class GraphValidTree {
+
+	public static void main(String[] args) {
+		int n = 5;
+		int [][]edges = {{0, 1}, {0, 2}, {0, 3}, {1, 4}};
+		boolean result = validTree(n, edges);
+		System.out.println(result);
+
+	}
+	
+	
+	    public static boolean validTree(int n, int[][] edges) {
+	        List<List<Integer>> graph = new ArrayList<>();
+	        if(edges.length != n - 1)
+	            return false;
+	        for(int i =0; i<n;i++){
+	            graph.add(new ArrayList<>());
+	        }
+	        for(int[] e :edges){
+	            graph.get(e[0]).add(e[1]);
+	            graph.get(e[1]).add(e[0]);
+	        }
+
+	        Queue<Integer> q = new LinkedList<>();
+	        boolean[] visited = new boolean[n];
+	        q.offer(0);
+	        visited[0] = true;
+	        int vCount = 0;
+	        while(!q.isEmpty()){
+	            int node = q.poll();
+	            vCount++;
+	            for(int i : graph.get(node)){
+	                if(!visited[i]){
+	                    visited[i] = true;
+	                    q.offer(i);
+	                }
+	            }
+	        }
+
+	        return vCount == n;
+	    }
+	
+
+}
