712eb0e7630a6e8ff12dd5b999d104702925a283
vimal <vimalkjoshi92@gmail.com>
2026-03-14 11:29:57 +0530

    Longest Valid Parentheses

---
+++ src/com/dsa/String/longestValidParentheses2PassScanning.java
@@ -0,0 +1,47 @@
+package com.dsa.String;
+
+public class longestValidParentheses2PassScanning {
+
+	public static void main(String[] args) {
+		String s = ")()())";
+		int ans = longestValidParentheses(s);
+		System.out.println(ans);
+
+	}
+
+	private static int longestValidParentheses(String s) {
+		int left = 0;
+		int right = 0;
+		int max = 0;
+		for(int i=0; i<s.length(); i++) {
+			if(s.charAt(i)=='(')
+				left++;
+			else
+				right++;
+			
+			if(left == right) {
+				max = Math.max(max,2*right);
+			}else if(right > left) {
+				left = 0;
+				right = 0;
+			}
+		}
+		left =0;
+		right = 0;
+		for(int i=s.length()-1; i>=0; i--) {
+			if(s.charAt(i) == '(')
+				left++;
+			else
+				right++;
+			
+			if(left == right) {
+				max = Math.max(max, 2*right);
+			}else if(left > right) {
+				left = 0;
+				right = 0;
+			}
+		}
+		return max;
+	}
+
+}
