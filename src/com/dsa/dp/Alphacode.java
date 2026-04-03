39709541512bec9ee4a3ae182c26fbb3ba354985
vimal <vimalkjoshi92@gmail.com>
2026-04-03 21:40:08 +0530

    alphacode

--- src/com/dsa/dp/Alphacode.java
+++ src/com/dsa/dp/Alphacode.java
@@ -43,3 +43,52 @@ public class Alphacode {
     }
 
 }
+
+/*
+ package main;
+
+import input.InputReader;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+public class law {
+    public void solve(int testNumber, InputReader in, PrintWriter out) {
+        while (true) {
+            String s = in.readString();
+            if (s.equals("0"))
+                break;
+
+            long[] dp = new long[s.length() + 1];
+            Arrays.fill(dp, -1);
+
+            out.println(calPath(s, 0, dp));
+        }
+    }
+
+    private long calPath(String s, int i, long[] dp) {
+        if (i >= s.length()) {
+            return 1;
+        }
+
+        if (s.charAt(i) == '0')
+            return 0;
+
+        if (dp[i] != -1) {
+            return dp[i];
+        }
+
+        long ways = calPath(s, i + 1, dp);
+
+        if (i + 1 < s.length()) {
+            int num = (s.charAt(i) - '0') * 10 + (s.charAt(i + 1) - '0');
+            if (num <= 26) {
+                ways += calPath(s, i + 2, dp);
+            }
+        }
+
+        return dp[i] = ways;
+    }
+}
+ 
+ */
