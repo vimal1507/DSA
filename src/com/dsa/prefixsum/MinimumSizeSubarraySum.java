5dacab2fd41c1fa727351ee2981226761e8f4d00
vimal <vimalkjoshi92@gmail.com>
2026-04-12 22:16:48 +0530

    Minimum Size Subarray Sum

--- src/com/dsa/prefixsum/MinimumSizeSubarraySum.java
+++ src/com/dsa/prefixsum/MinimumSizeSubarraySum.java
@@ -4,29 +4,28 @@ public class MinimumSizeSubarraySum {
 
 	public static void main(String[] args) {
 		int target = 7;
-		int[] nums = {2,3,1,2,4,3};
+		int[] nums = { 2, 3, 1, 2, 4, 3 };
 		System.out.println(minSubArrayLen(target, nums));
 
 	}
 
 	public static int minSubArrayLen(int target, int[] nums) {
-		int f = 0;
-		int s = 1;
-		int sum = nums[f];
-		int result = Integer.MAX_VALUE;
-		while (s < nums.length) {
-			if (sum >= target) {
-				sum += nums[s];
-				s++;
-				System.out.println(s-f);
-			} else {
-				sum = nums[s-1];
-				System.out.println(s-f);
-				result = Math.min(result, s - f);
-				f = s;
-				s++;
+		int i = 0;
+		int j = 0;
+		int length = Integer.MAX_VALUE;
+		int sum = 0;
+
+		while (j < nums.length) {
+			sum += nums[j];
+
+			while (sum >= target) {
+				length = Math.min(length, j - i + 1);
+				sum -= nums[i];
+				i++;
 			}
+			j++;
 		}
-		return result;
+
+		return length == Integer.MAX_VALUE ? 0 : length;
 	}
 }
