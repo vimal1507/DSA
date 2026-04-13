ac224d8c805ea93c8d54c9f2e6cb9c7540fdb8ae
vimal <vimalkjoshi92@gmail.com>
2026-04-13 16:45:16 +0530

    meeting room

---
+++ src/com/dsa/prefixsum/MeetingRoomII.java
@@ -0,0 +1,37 @@
+package com.dsa.prefixsum;
+
+import java.util.List;
+
+public class MeetingRoomII {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+	    public static int minMeetingRooms(List<Interval> intervals) {
+	        int[] time = new int[1000001];
+	        for(int i=0; i<intervals.size(); i++){
+	            Interval t = intervals.get(i);
+	            time[t.start] += 1;
+	            time[t.end] += -1;
+	        }
+	        int count = 0;
+	        int res = 0;
+	        for(int x : time){
+	            count += x;
+	            res = Math.max(res,count);
+	        }
+	        return res;
+	    }
+	}
+
+
+ 
+   class Interval {
+      public int start, end;
+      public Interval(int start, int end) {
+          this.start = start;
+          this.end = end;
+      }
+  }
