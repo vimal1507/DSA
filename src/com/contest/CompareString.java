bc6911cdf12844cceadb66a40fafb06fefdc801b
vimal <vimalkjoshi92@gmail.com>
2026-04-19 12:04:27 +0530

    stack

--- src/com/contest/CompareString.java
+++ src/com/contest/CompareString.java
@@ -20,5 +20,35 @@ public class CompareString {
         }
         return sb.toString();
     }
+	
+	/*
+	 private String convert(String s){
+        StringBuilder sb = new StringBuilder();
+        Stack<Character> st = new Stack<>();
+        for(int i=0; i<s.length(); i++){
+            st.push(s.charAt(i));
+            String temp = "";
+            if(st.size() >= 4){
+                temp = st.pop()+temp;
+                temp = st.pop()+temp;
+                temp = st.pop()+temp;
+                temp = st.pop()+temp;
+            }
+            if(temp.equals("(xx)")){
+                st.push('x');
+                st.push('x');
+            }else if(temp !="" &&!temp.equals("(xx)")){
+                st.push(temp.charAt(0));
+                st.push(temp.charAt(1));
+                st.push(temp.charAt(2));
+                st.push(temp.charAt(3));
+            }
+        }
+        for(char z : st){
+            sb.append(z);
+        }
+        return sb.toString();
+    }
+	 */
 
 }
