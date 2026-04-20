8d1bd8610efebe6c53161d704e695414f829ac86
vimal <vimalkjoshi92@gmail.com>
2026-04-20 23:27:50 +0530

    Linked List Cycle II 2nd method

--- src/com/dsa/miscellaneous/LinkedListCycleII.java
+++ src/com/dsa/miscellaneous/LinkedListCycleII.java
@@ -48,3 +48,34 @@ public class LinkedListCycleII {
         return fast;
     }
 }
+ 
+ 
+ /*
+ 	
+
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        HashSet<ListNode> s = new HashSet<>();
+        ListNode temp = head;
+    
+        while(temp != null){
+            if(s.contains(temp)){
+                return temp;
+            }
+            s.add(temp);
+            temp = temp.next;
+        }
+        return null;
+    }
+}
+ */
