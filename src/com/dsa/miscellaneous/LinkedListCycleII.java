e4f0fece8d7fee5b469cc830f064bd8ac2920429
vimal <vimalkjoshi92@gmail.com>
2026-04-20 23:16:35 +0530

    Linked List Cycle II

---
+++ src/com/dsa/miscellaneous/LinkedListCycleII.java
@@ -0,0 +1,50 @@
+package com.dsa.miscellaneous;
+
+public class LinkedListCycleII {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+
+ // Definition for singly-linked list.
+  class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) {
+          val = x;
+          next = null;
+      }
+  }
+
+ class Solution {
+    public ListNode detectCycle(ListNode head) {
+        if (head == null || head.next == null) return null;
+        ListNode slow = head;
+        ListNode fast = head;
+        boolean flag = false;
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+
+            if(fast == slow){
+                flag = true;
+                break;
+            }
+        }
+        if(!flag){
+            return null;
+        }
+
+        slow = head;
+        while(slow != fast){
+            slow = slow.next;
+            fast = fast.next;
+        }
+       
+        return fast;
+    }
+}
